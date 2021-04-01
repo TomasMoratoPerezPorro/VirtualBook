dabcd853dcef82d4de91db189d01ad495cab8cc2
TomasMoratoPerezPorro <49519581+TomasMoratoPerezPorro@users.noreply.github.com>
2021-04-01 13:10:26 +0200

    Hard Pages

--- samples/magazine/js/magazine.js
+++ samples/magazine/js/magazine.js
@@ -4,13 +4,17 @@
 
 function addPage(page, book) {
   var id,
-    pages = book.turn('pages');
+    pages = book.turn("pages");
 
+  if (page === 1 || page === 2 || page === 15 || page === 16) {
+    var element = $('<div class="hard" />', {});
+  } else {
+    var element = $("<div />", {});
+  }
   // Create a new element for this page
-  var element = $('<div />', {});
 
   // Add the page to the flipbook
-  if (book.turn('addPage', element, page)) {
+  if (book.turn("addPage", element, page)) {
     // Add the initial HTML
     // It will contain a loader indicator and a gradient
     element.html('<div class="gradient"></div><div class="loader"></div>');
@@ -23,7 +27,7 @@ function addPage(page, book) {
 function loadPage(page, pageElement) {
   // Create an image element
 
-  var img = $('<img />');
+  var img = $("<img />");
 
   img.mousedown(function (e) {
     e.preventDefault();
@@ -31,7 +35,7 @@ function loadPage(page, pageElement) {
 
   img.load(function () {
     // Set the size
-    $(this).css({ width: '100%', height: '100%' });
+    $(this).css({ width: "100%", height: "100%" });
 
     // Add the image to the page after loaded
 
@@ -39,12 +43,12 @@ function loadPage(page, pageElement) {
 
     // Remove the loader indicator
 
-    pageElement.find('.loader').remove();
+    pageElement.find(".loader").remove();
   });
 
   // Load the page
 
-  img.attr('src', 'pages/' + page + '.png');
+  img.attr("src", "pages/" + page + ".png");
 
   /* loadRegions(page, pageElement); */
 }
@@ -53,13 +57,13 @@ function loadPage(page, pageElement) {
 
 function zoomTo(event) {
   setTimeout(function () {
-    if ($('.magazine-viewport').data().regionClicked) {
-      $('.magazine-viewport').data().regionClicked = false;
+    if ($(".magazine-viewport").data().regionClicked) {
+      $(".magazine-viewport").data().regionClicked = false;
     } else {
-      if ($('.magazine-viewport').zoom('value') == 1) {
-        $('.magazine-viewport').zoom('zoomIn', event);
+      if ($(".magazine-viewport").zoom("value") == 1) {
+        $(".magazine-viewport").zoom("zoomIn", event);
       } else {
-        $('.magazine-viewport').zoom('zoomOut');
+        $(".magazine-viewport").zoom("zoomOut");
       }
     }
   }, 1);
@@ -68,7 +72,7 @@ function zoomTo(event) {
 // Load regions
 
 function loadRegions(page, element) {
-  $.getJSON('pages/' + page + '-regions.json').done(function (data) {
+  $.getJSON("pages/" + page + "-regions.json").done(function (data) {
     $.each(data, function (key, region) {
       addRegion(region, element);
     });
@@ -78,19 +82,19 @@ function loadRegions(page, element) {
 // Add region
 
 function addRegion(region, pageElement) {
-  var reg = $('<div />', { class: 'region  ' + region['class'] }),
-    options = $('.magazine').turn('options'),
+  var reg = $("<div />", { class: "region  " + region["class"] }),
+    options = $(".magazine").turn("options"),
     pageWidth = options.width / 2,
     pageHeight = options.height;
 
   reg
     .css({
-      top: Math.round((region.y / pageHeight) * 100) + '%',
-      left: Math.round((region.x / pageWidth) * 100) + '%',
-      width: Math.round((region.width / pageWidth) * 100) + '%',
-      height: Math.round((region.height / pageHeight) * 100) + '%'
+      top: Math.round((region.y / pageHeight) * 100) + "%",
+      left: Math.round((region.x / pageWidth) * 100) + "%",
+      width: Math.round((region.width / pageWidth) * 100) + "%",
+      height: Math.round((region.height / pageHeight) * 100) + "%",
     })
-    .attr('region-data', $.param(region.data || ''));
+    .attr("region-data", $.param(region.data || ""));
 
   reg.appendTo(pageElement);
 }
@@ -100,14 +104,14 @@ function addRegion(region, pageElement) {
 function regionClick(event) {
   var region = $(event.target);
 
-  if (region.hasClass('region')) {
-    $('.magazine-viewport').data().regionClicked = true;
+  if (region.hasClass("region")) {
+    $(".magazine-viewport").data().regionClicked = true;
 
     setTimeout(function () {
-      $('.magazine-viewport').data().regionClicked = false;
+      $(".magazine-viewport").data().regionClicked = false;
     }, 100);
 
-    var regionType = $.trim(region.attr('class').replace('region', ''));
+    var regionType = $.trim(region.attr("class").replace("region", ""));
 
     return processRegion(region, regionType);
   }
@@ -116,26 +120,26 @@ function regionClick(event) {
 // Process the data of every region
 
 function processRegion(region, regionType) {
-  data = decodeParams(region.attr('region-data'));
+  data = decodeParams(region.attr("region-data"));
 
   switch (regionType) {
-    case 'link':
+    case "link":
       window.open(data.url);
 
       break;
-    case 'zoom':
+    case "zoom":
       var regionOffset = region.offset(),
-        viewportOffset = $('.magazine-viewport').offset(),
+        viewportOffset = $(".magazine-viewport").offset(),
         pos = {
           x: regionOffset.left - viewportOffset.left,
-          y: regionOffset.top - viewportOffset.top
+          y: regionOffset.top - viewportOffset.top,
         };
 
-      $('.magazine-viewport').zoom('zoomIn', pos);
+      $(".magazine-viewport").zoom("zoomIn", pos);
 
       break;
-    case 'to-page':
-      $('.magazine').turn('page', data.page);
+    case "to-page":
+      $(".magazine").turn("page", data.page);
 
       break;
   }
@@ -144,45 +148,45 @@ function processRegion(region, regionType) {
 // Load large page
 
 function loadLargePage(page, pageElement) {
-  var img = $('<img />');
+  var img = $("<img />");
 
   img.load(function () {
-    var prevImg = pageElement.find('img');
-    $(this).css({ width: '100%', height: '100%' });
+    var prevImg = pageElement.find("img");
+    $(this).css({ width: "100%", height: "100%" });
     $(this).appendTo(pageElement);
     prevImg.remove();
   });
 
   // Loadnew page
 
-  img.attr('src', 'pages/' + page + '-large.png');
+  img.attr("src", "pages/" + page + "-large.png");
 }
 
 // Load small page
 
 function loadSmallPage(page, pageElement) {
-  var img = pageElement.find('img');
+  var img = pageElement.find("img");
 
-  img.css({ width: '100%', height: '100%' });
+  img.css({ width: "100%", height: "100%" });
 
-  img.unbind('load');
+  img.unbind("load");
   // Loadnew page
 
-  img.attr('src', 'pages/' + page + '.png');
+  img.attr("src", "pages/" + page + ".png");
 }
 
 // http://code.google.com/p/chromium/issues/detail?id=128488
 
 function isChrome() {
-  return navigator.userAgent.indexOf('Chrome') != -1;
+  return navigator.userAgent.indexOf("Chrome") != -1;
 }
 
 function disableControls(page) {
-  if (page == 1) $('.previous-button').hide();
-  else $('.previous-button').show();
+  if (page == 1) $(".previous-button").hide();
+  else $(".previous-button").show();
 
-  if (page == $('.magazine').turn('pages')) $('.next-button').hide();
-  else $('.next-button').show();
+  if (page == $(".magazine").turn("pages")) $(".next-button").hide();
+  else $(".next-button").show();
 }
 
 // Set the width and height for the viewport
@@ -190,117 +194,117 @@ function disableControls(page) {
 function resizeViewport() {
   var width = $(window).width(),
     height = $(window).height(),
-    options = $('.magazine').turn('options');
+    options = $(".magazine").turn("options");
 
-  $('.magazine').removeClass('animated');
+  $(".magazine").removeClass("animated");
 
-  $('.magazine-viewport')
+  $(".magazine-viewport")
     .css({
       width: width,
-      height: height
+      height: height,
     })
-    .zoom('resize');
+    .zoom("resize");
 
-  if ($('.magazine').turn('zoom') == 1) {
+  if ($(".magazine").turn("zoom") == 1) {
     var bound = calculateBound({
       width: options.width,
       height: options.height,
       boundWidth: Math.min(options.width, width),
-      boundHeight: Math.min(options.height, height)
+      boundHeight: Math.min(options.height, height),
     });
 
     if (bound.width % 2 !== 0) bound.width -= 1;
 
     if (
-      bound.width != $('.magazine').width() ||
-      bound.height != $('.magazine').height()
+      bound.width != $(".magazine").width() ||
+      bound.height != $(".magazine").height()
     ) {
-      $('.magazine').turn('size', bound.width, bound.height);
+      $(".magazine").turn("size", bound.width, bound.height);
 
-      if ($('.magazine').turn('page') == 1) $('.magazine').turn('peel', 'br');
+      if ($(".magazine").turn("page") == 1) $(".magazine").turn("peel", "br");
 
-      $('.next-button').css({
+      $(".next-button").css({
         height: bound.height,
-        backgroundPosition: '-38px ' + (bound.height / 2 - 32 / 2) + 'px'
+        backgroundPosition: "-38px " + (bound.height / 2 - 32 / 2) + "px",
       });
-      $('.previous-button').css({
+      $(".previous-button").css({
         height: bound.height,
-        backgroundPosition: '-4px ' + (bound.height / 2 - 32 / 2) + 'px'
+        backgroundPosition: "-4px " + (bound.height / 2 - 32 / 2) + "px",
       });
     }
 
-    $('.magazine').css({ top: -bound.height / 2, left: -bound.width / 2 });
+    $(".magazine").css({ top: -bound.height / 2, left: -bound.width / 2 });
   }
 
-  var magazineOffset = $('.magazine').offset(),
-    boundH = height - magazineOffset.top - $('.magazine').height(),
-    marginTop = (boundH - $('.thumbnails > div').height()) / 2;
+  var magazineOffset = $(".magazine").offset(),
+    boundH = height - magazineOffset.top - $(".magazine").height(),
+    marginTop = (boundH - $(".thumbnails > div").height()) / 2;
 
   if (marginTop < 0) {
-    $('.thumbnails').css({ height: 1 });
+    $(".thumbnails").css({ height: 1 });
   } else {
-    $('.thumbnails').css({ height: boundH });
-    $('.thumbnails > div').css({ marginTop: marginTop });
+    $(".thumbnails").css({ height: boundH });
+    $(".thumbnails > div").css({ marginTop: marginTop });
   }
 
-  if (magazineOffset.top < $('.made').height()) $('.made').hide();
-  else $('.made').show();
+  if (magazineOffset.top < $(".made").height()) $(".made").hide();
+  else $(".made").show();
 
-  $('.magazine').addClass('animated');
+  $(".magazine").addClass("animated");
 }
 
 // Number of views in a flipbook
 
 function numberOfViews(book) {
-  return book.turn('pages') / 2 + 1;
+  return book.turn("pages") / 2 + 1;
 }
 
 // Current view in a flipbook
 
 function getViewNumber(book, page) {
-  return parseInt((page || book.turn('page')) / 2 + 1, 10);
+  return parseInt((page || book.turn("page")) / 2 + 1, 10);
 }
 
 function moveBar(yes) {
   if (Modernizr && Modernizr.csstransforms) {
-    $('#slider .ui-slider-handle').css({ zIndex: yes ? -1 : 10000 });
+    $("#slider .ui-slider-handle").css({ zIndex: yes ? -1 : 10000 });
   }
 }
 
 function setPreview(view) {
   var previewWidth = 112,
     previewHeight = 73,
-    previewSrc = 'pages/preview.png',
-    preview = $(_thumbPreview.children(':first')),
+    previewSrc = "pages/preview.png",
+    preview = $(_thumbPreview.children(":first")),
     numPages =
-      view == 1 || view == $('#slider').slider('option', 'max') ? 1 : 2,
+      view == 1 || view == $("#slider").slider("option", "max") ? 1 : 2,
     width = numPages == 1 ? previewWidth / 2 : previewWidth;
 
-  _thumbPreview.addClass('no-transition').css({
+  _thumbPreview.addClass("no-transition").css({
     width: width + 15,
     height: previewHeight + 15,
     top: -previewHeight - 30,
-    left: ($($('#slider').children(':first')).width() - width - 15) / 2
+    left: ($($("#slider").children(":first")).width() - width - 15) / 2,
   });
 
   preview.css({
     width: width,
-    height: previewHeight
+    height: previewHeight,
   });
 
   if (
-    preview.css('background-image') === '' ||
-    preview.css('background-image') == 'none'
+    preview.css("background-image") === "" ||
+    preview.css("background-image") == "none"
   ) {
-    preview.css({ backgroundImage: 'url(' + previewSrc + ')' });
+    preview.css({ backgroundImage: "url(" + previewSrc + ")" });
 
     setTimeout(function () {
-      _thumbPreview.removeClass('no-transition');
+      _thumbPreview.removeClass("no-transition");
     }, 0);
   }
 
   preview.css({
-    backgroundPosition: '0px -' + (view - 1) * previewHeight + 'px'
+    backgroundPosition: "0px -" + (view - 1) * previewHeight + "px",
   });
 }
 
@@ -313,12 +317,12 @@ function largeMagazineWidth() {
 // decode URL Parameters
 
 function decodeParams(data) {
-  var parts = data.split('&'),
+  var parts = data.split("&"),
     d,
     obj = {};
 
   for (var i = 0; i < parts.length; i++) {
-    d = parts[i].split('=');
+    d = parts[i].split("=");
     obj[decodeURIComponent(d[0])] = decodeURIComponent(d[1]);
   }
 
